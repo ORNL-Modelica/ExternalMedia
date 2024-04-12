a7f1b48810f68230b6b42c6f032d86be5269cfcb
Scott Greenwood <greenwoodms@ornl.gov>
2024-04-12 16:24:04 -0400

    capitalize vs

--- Projects/BuildLib-Dymola-VS2022.bat
+++ Projects/BuildLib-Dymola-VS2022.bat
@@ -9,7 +9,7 @@ call BuildLib-VS
 
 echo "Copying files to External Media 3.2.1"
 if not exist "..\Modelica\ExternalMedia 3.2.1\Resources\Library\win64\" mkdir "..\Modelica\ExternalMedia 3.2.1\Resources\Library\win64\"
-copy ExternalMediaLib.lib "..\Modelica\ExternalMedia 3.2.1\Resources\Library\win64\ExternalMediaLib.Dymola-vs2022.lib"
+copy ExternalMediaLib.lib "..\Modelica\ExternalMedia 3.2.1\Resources\Library\win64\ExternalMediaLib.Dymola-VS2022.lib"
 copy ExternalMediaLib.lib "..\Modelica\ExternalMedia 3.2.1\Resources\Library\win64\ExternalMediaLib.lib"
 del ExternalMediaLib.lib
 copy Sources\externalmedialib.h "..\Modelica\ExternalMedia 3.2.1\Resources\Include"
